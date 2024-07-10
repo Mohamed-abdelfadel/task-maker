f72845768e3f490370549c5402b6087a1763ac73
Mohamed <mohamed.yasser99999@gmail.com>
2024-07-10 10:16:51 +0300

    feat(users): create users cycle

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        User::factory()->count(5)->create();
+
+    }
+}
