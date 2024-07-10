14f015d481cb0a3f416454ed2c0ad55da0fef8ed
Mohamed <mohamed.yasser99999@gmail.com>
2024-07-11 02:25:40 +0300

    test(user): create user unit testing

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testUsersList(): void
+    {
+        $response = $this->get('/users');
+
+        $response->assertStatus(200);
+    }
+
+    public function testUserCreate(): void
+    {
+        $response = $this->get('/users/create');
+
+        $response->assertStatus(200);
+    }
+
+    public function testUserDestroy(): void
+    {
+        $adminRole = new Role();
+        $adminRole->name = 'admin';
+        $adminRole->save();
+        $adminRoleId = $adminRole->id;
+
+        $user = new User();
+        $user->name = "Mohamed Yasser";
+        $user->email = "mohamedy.dev@gmail.com";
+        $user->role_id = $adminRoleId;
+        $user->avatar = 'avatar.png';
+        $user->email_verified_at = now();
+        $user->password = Hash::make('1234');
+        $user->save();
+        $id = $user->id;
+        $response = $this->delete("/users/destroy/$id");
+
+        $response->assertRedirect('/users');
+        $this->assertDatabaseMissing('users', ['id' => $id]);
+    }
+
+
+}
