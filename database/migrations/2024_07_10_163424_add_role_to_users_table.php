3c4aebea935a30b2c4eec898e13be5c9f11fa3df
Mohamed <mohamed.yasser99999@gmail.com>
2024-07-11 04:48:19 +0300

    refactor(roles): refactor roles cycle

--- database/migrations/2024_07_10_163424_add_role_to_users_table.php
+++ database/migrations/2024_07_10_163424_add_role_to_users_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->after('email')->index() ;
+            $table->foreignId('role_id')->after('email')->index();
             $table->foreign('role_id')->references('id')->on('roles')->onUpdate('CASCADE')->onDelete('RESTRICT');
 
         });
